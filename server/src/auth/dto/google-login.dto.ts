55935d7a2c5f671e26c4eeabe6845cfe76a09b63
CristianoMends <mendescristiano012@gmail.com>
2025-07-12 16:04:31 -0300

    apdate

--- server/src/auth/dto/google-login.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class GoogleLoginDto {
-    @IsNotEmpty({ message: 'O token do Google é obrigatório.' })
-    @IsString({ message: 'O token deve ser uma string.' })
-    code: string;
-}
