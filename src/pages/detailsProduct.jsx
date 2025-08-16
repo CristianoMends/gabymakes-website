535abc348c573a4f549fe01859e7a622285d8f36
CristianoMends <mendescristiano012@gmail.com>
2025-08-15 21:07:33 -0300

    fix detailsProduct

--- src/pages/detailsProduct.jsx
+++ src/pages/detailsProduct.jsx
@@ -14,7 +14,6 @@ import { MdOutlineShoppingBag } from "react-icons/md";
 import { BiPurchaseTagAlt } from "react-icons/bi";
 import RelatedProducts from '../components/relatedProducts';
 
-
 const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 export default function ProductDetailPage() {
